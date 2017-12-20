738f2a3e390a14b1388dfad322a6a7b4cf9680cc
Nick Haglind <nhaglind@gmail.com>
2017-12-20 11:17:10 -0600

    nyt and wapo only accepted domains

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let first_button = document.querySelector('#first_button');
 let second_button = document.querySelector('#second_button');
 let fourth_button = document.querySelector('#fourth_button');
 
+let approvedCEDomains = `site%3Anytimes.com%2Cwashingtonpost.com`
 
 function taunt() {
   let first_answer = document.querySelector('#first_answer').value;
@@ -17,11 +18,7 @@ function taunt_2() {
 
 function taunt_4() {
   let fourth_answer = document.querySelector('#fourth_answer').value;
-  document.querySelector('.fourth_response').innerHTML = (`<p>Figured you'd look for that. Here are some reputable places to look.</p>
-  <ul>
-  <li>NYT: <a href="https://duckduckgo.com/?q=site%3Ahttps%3A%2F%2Fwww.nytimes.com+${fourth_answer}&t=hf&ia=web">${fourth_answer}</a>
-  <li>Wapo: <a href="https://duckduckgo.com/?q=site%3Ahttps%3A%2F%2Fwww.washingtonpost.com+${fourth_answer}&t=hf&ia=web">${fourth_answer}</a>
-  </ul>`);
+  document.querySelector('.fourth_response').href = `https://duckduckgo.com/?q=${approvedCEDomains}+${fourth_answer}&ia=web`
 }
 
 first_button.addEventListener('click', taunt);
