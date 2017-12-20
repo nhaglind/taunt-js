f2cf462d4c383ee540b44a54dd10c1e7ab9f1f2a
Nick Haglind <nhaglind@gmail.com>
2017-12-20 11:03:44 -0600

    make them take you to link directly

--- script.js
+++ script.js
@@ -1,44 +1,29 @@
 let first_button = document.querySelector('#first_button');
 let second_button = document.querySelector('#second_button');
-let third_button = document.querySelector('#third_button');
 let fourth_button = document.querySelector('#fourth_button');
 
 
 function taunt() {
   let first_answer = document.querySelector('#first_answer').value;
   (first_answer == "") ? first_answer = 'dog food': first_answer;
-  document.querySelector('.first_response').innerHTML = `Wow, I can't believe you're searching for ${first_answer}. Search for <a href="https://duckduckgo.com/?q=${first_answer}">${first_answer}</a>.`;
+  document.querySelector('.first_response').href = `https://duckduckgo.com/?q=${first_answer}`;
 }
 
 function taunt_2() {
   let second_answer = document.querySelector('#second_answer').value;
   (second_answer == "") ? second_answer = 'Taylor Swift': second_answer;
-  document.querySelector('.second_response').innerHTML = `What a weird search, here you go. Image search for <a href="https://duckduckgo.com/?q=${second_answer}&iax=images&ia=images">${second_answer}</a>.`;
-}
-
-function taunt_3() {
-  let third_answer = document.querySelector('#third_answer').value;
-  let yen = third_answer * 113.02;
-  let euro = third_answer * 0.85;
-  document.querySelector('.third_response').innerHTML = (`<p>Wow, much poor. Here is a rough estimate of your money in these currencies.</p> 
-  <ul>
-  <li>Japanese Yen: ${yen.toFixed(2)}</li>
-  <li>Euro: ${euro.toFixed(2)}</li>
-  </ul>`);
+  document.querySelector('.second_response').href = `https://duckduckgo.com/?q=${second_answer}&iax=images&ia=images`;
 }
 
 function taunt_4() {
   let fourth_answer = document.querySelector('#fourth_answer').value;
   document.querySelector('.fourth_response').innerHTML = (`<p>Figured you'd look for that. Here are some reputable places to look.</p>
   <ul>
-  <li>NYT:
-  <li>Wapo:
-  <li>...
-  <li>...
+  <li>NYT: <a href="https://duckduckgo.com/?q=site%3Ahttps%3A%2F%2Fwww.nytimes.com+${fourth_answer}&t=hf&ia=web">${fourth_answer}</a>
+  <li>Wapo: <a href="https://duckduckgo.com/?q=site%3Ahttps%3A%2F%2Fwww.washingtonpost.com+${fourth_answer}&t=hf&ia=web">${fourth_answer}</a>
   </ul>`);
 }
 
 first_button.addEventListener('click', taunt);
 second_button.addEventListener('click', taunt_2);
-third_button.addEventListener('click', taunt_3);
 fourth_button.addEventListener('click', taunt_4);
